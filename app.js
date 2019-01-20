a89e37414d710f1ceb29fd650c5d5bd8c5c62d87
Pedro <pedroteixeirabisognin@gmail.com>
2019-01-20 13:40:01 -0200

    Fazendo mais algumas alterações

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server')
 
-app.set('view engine', 'ejs');
+var rotaNoticias = require('./app/routes/noticias')(app);;
 
-app.get('/',function(req,res){
+var rotaHome = require('./app/routes/home')(app);;
 
-    res.render("home/index")
-});
-
-app.get('/formulario_inclusao_noticia',function(req,res){
-
-    res.render("admin/form_add_noticia")
-});
-
-
-app.get('/noticias',function(req,res){
-
-    res.render("noticias/noticias")
-});
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticias')(app);;
 
 app.listen(3000, function(){
 
-    console.log("Servidor rodando com Express");
+    console.log("Servidor ON");
     
 });
 
