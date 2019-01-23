34eb12812570986959789f2c1fd1772049eceb79
Pedro <pedroteixeirabisognin@gmail.com>
2019-01-22 23:44:42 -0200

    Inserida conexão com mysql

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var app = require('./config/server')
 
-var rotaNoticias = require('./app/routes/noticias')(app);;
+var rotaNoticias = require('./app/routes/noticias')(app);
 
-var rotaHome = require('./app/routes/home')(app);;
+var rotaHome = require('./app/routes/home')(app);
 
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticias')(app);;
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticias')(app);
 
 app.listen(3000, function(){
 
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,23 @@
 module.exports = function (app) {
     app.get('/noticias', function (req, res) {
 
-        res.render("noticias/noticias")
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: '',
+            database: 'portal_noticias'
+
+        });
+
+        connection.query('SELECT * FROM noticias', function (erro, result) {
+            console.log(erro);
+            
+         res.render("noticias/noticias",{noticias:result});
+        });
+
     });
 }
