497db627fdd00c65ca046a3f0bcbf8544d0d0624
Pedro <pedroteixeirabisognin@gmail.com>
2019-01-30 00:27:19 -0200

    Refatorando o banco de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,23 +1,16 @@
+var dbConnection = require('../../config/dbConnection')
 module.exports = function (app) {
     app.get('/noticias', function (req, res) {
 
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-
-            host: 'localhost',
-            port: 3306,
-            user: 'root',
-            password: '',
-            database: 'portal_noticias'
-
-        });
+        var connection = dbConnection();
 
         connection.query('SELECT * FROM noticias', function (erro, result) {
             console.log(erro);
-            
-         res.render("noticias/noticias",{noticias:result});
+            //PARA ENVIAR O RESULTADO SEM USAR O RENDER
+            //res.send(result);   
+            res.render("noticias/noticias",{noticias:result});
         });
+        
 
     });
 }
---
+++ config/dbConnection.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+module.exports = function () {
+
+    return mysql.createConnection({
+
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'portal_noticias'
+
+    });
+
+}
